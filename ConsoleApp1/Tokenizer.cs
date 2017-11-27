c38b70b6723b4d0666b445ce5c75d9fa0f4bef26
SourceTreeIsGay <huozuyinshua@gmail.com>
2017-11-27 12:31:19 +0900

    tokenizer

---
+++ ConsoleApp1/Tokenizer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+    internal class Tokenizer
+    {
+        static string[] readFile(string[] lineList)
+        {
+            try
+            {
+
+                 lineList = File.ReadAllLines(@"Input.txt");
+                foreach (string line in lineList)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.ToString());
+            }
+
+            return lineList;
+        }
+
+        
+    }
